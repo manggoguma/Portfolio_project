58ac5b26e8692d3fe573da463311a1ee2e5d70ee
haeunNam <nhe0103@gamail.com>
2024-06-07 14:46:58 +0900

    tt

--- contact.js
+++ contact.js
@@ -1,13 +1,8 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // 전송 버튼 요소를 가져옵니다.
-    var sendButton = document.querySelector('.sendButton');
-    // 결과 메시지 요소를 가져옵니다.
-    var submitResult = document.querySelector('.submitResult');
-
-    // 버튼 클릭 시 동작하는 함수를 정의합니다.
-    sendButton.addEventListener('click', function() {
-        // 결과 메시지를 보여줍니다.
-        submitResult.style.display = 'block';
-    });
+document.querySelector('.sendButton').addEventListener('click', function() {
+    var resultElement = document.querySelector('.submitResult');
+    if (resultElement.style.display === 'none' || resultElement.style.display === '') {
+        resultElement.style.display = 'block';
+    } else {
+        resultElement.style.display = 'none';
+    }
 });
-
